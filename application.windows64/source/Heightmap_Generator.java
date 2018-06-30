95a96d12135078e7f7a6a1685ed69714c17694d5
Seth <wheelersc@cox.net>
2018-06-29 23:31:47 -0700

    Initial commit

---
+++ application.windows64/source/Heightmap_Generator.java
@@ -0,0 +1,68 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Heightmap_Generator extends PApplet {
+
+int cols, rows;
+
+float flying = 0;
+
+float[][] terrain;
+
+float SALT = .005f;
+
+public void setup() {
+  
+  cols = width;
+  rows = height;
+  terrain = new float[cols][rows];
+}
+
+
+public void draw() {
+  background(0);
+  loadPixels();
+  float yoff  = 0;
+  for (int y = 1; y < rows - 1; y++) {
+    float xoff = 0;
+    for (int x = 1; x < cols - 1; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -500, 500);
+      xoff += SALT;
+
+      terrain[x][y] -= terrain[x][y]/5;
+
+      if (terrain[x][y] <= 25) {
+        terrain[x][y] += (terrain[x-1][y] + 
+          terrain[x+1][y] + 
+          terrain[x][y-1] + 
+          terrain[x][y+1]) / 256;
+      }
+      int index = x + y * cols;
+      pixels[index] = color(terrain[x][y]);
+    }
+    yoff += SALT;
+  }
+
+  updatePixels();
+}
+  public void settings() {  size(900, 900); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Heightmap_Generator" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ application.windows64/source/Heightmap_Generator.pde
@@ -0,0 +1,42 @@
+int cols, rows;
+
+float flying = 0;
+
+float[][] terrain;
+
+float SALT = .005;
+
+void setup() {
+  size(900, 900);
+  cols = width;
+  rows = height;
+  terrain = new float[cols][rows];
+}
+
+
+void draw() {
+  background(0);
+  loadPixels();
+  float yoff  = 0;
+  for (int y = 1; y < rows - 1; y++) {
+    float xoff = 0;
+    for (int x = 1; x < cols - 1; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, -500, 500);
+      xoff += SALT;
+
+      terrain[x][y] -= terrain[x][y]/5;
+
+      if (terrain[x][y] <= 25) {
+        terrain[x][y] += (terrain[x-1][y] + 
+          terrain[x+1][y] + 
+          terrain[x][y-1] + 
+          terrain[x][y+1]) / 256;
+      }
+      int index = x + y * cols;
+      pixels[index] = color(terrain[x][y]);
+    }
+    yoff += SALT;
+  }
+
+  updatePixels();
+}
